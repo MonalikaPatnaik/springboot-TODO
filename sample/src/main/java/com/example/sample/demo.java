925278daf53020c4f99747100f59d66ddced4a30
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-12-14 01:20:43 +0530

    initialized-basic-springboot-application

---
+++ sample/src/main/java/com/example/sample/demo.java
@@ -0,0 +1,11 @@
+package com.example.sample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class demo {
+    public void print(){
+        System.out.println("this is demo class");
+    }
+
+}
---
+++ sample/src/main/java/com/example/sample/main.java
@@ -0,0 +1,11 @@
+package com.example.sample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class main {
+    private demo Demo;
+    public void print(){
+        Demo.print();
+    }
+}
